46da699cf8586cd51e72bf98220136c2d4727f7e
makiwebdeveloper <makiwebdeveloper@gmail.com>
2023-07-17 16:03:20 +0200

    project schema

--- src/sanity/sanity.config.ts
+++ src/sanity/sanity.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "sanity";
 import { deskTool } from "sanity/desk";
 import { visionTool } from "@sanity/vision";
+import schemas from "./schemas";
 
 const sanityConfig = defineConfig({
   title: "makiwebdeveloper portfolio",
@@ -9,6 +10,9 @@ const sanityConfig = defineConfig({
   plugins: [deskTool(), visionTool()],
   apiVersion: "2023-07-17",
   basePath: "/admin",
+  schema: {
+    types: schemas,
+  },
 });
 
 export default sanityConfig;
--- src/sanity/schemas/project.schema.ts
+++ src/sanity/schemas/project.schema.ts
@@ -1,1 +1,51 @@
-const project = {};
+const projectSchema = {
+  name: "project",
+  title: "Project",
+  type: "document",
+  fields: [
+    {
+      name: "name",
+      title: "Name",
+      type: "string",
+    },
+    {
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: { source: "name" },
+    },
+    {
+      name: "image",
+      title: "Image",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+      fields: [
+        {
+          name: "alt",
+          title: "Alt",
+          type: "string",
+        },
+      ],
+    },
+    {
+      name: "projectUrl",
+      title: "Project URL",
+      type: "url",
+    },
+    {
+      name: "githubUrl",
+      title: "GitHub URL",
+      type: "url",
+    },
+    {
+      name: "content",
+      title: "Content",
+      type: "array",
+      of: [{ type: "block" }],
+    },
+  ],
+};
+
+export default projectSchema;
